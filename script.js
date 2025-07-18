fc18d9037bd2897785ed7054703854205b1b19ab
BoniSec <bonimghe@gmail.com>
2025-07-18 18:23:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -20,8 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Smooth scrolling for nav links
   const links = document.querySelectorAll("nav a");
   links.forEach(link => {
-    link.addEventListener("click", (e) => {
-      e.preventDefault();
+    
       const section = document.querySelector(link.getAttribute("href"));
       if (section) {
         section.scrollIntoView({
